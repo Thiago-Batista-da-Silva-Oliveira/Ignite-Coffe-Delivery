7de23dd5202de54f1f285feca4461a22b624821b
Thiago <gmoliveira69@gmail.com>
2022-12-11 14:25:22 -0300

    Tags

--- src/pages/home/components/CoffeeCard/index.tsx
+++ src/pages/home/components/CoffeeCard/index.tsx
@@ -1,10 +1,14 @@
 import teste from '../../../../assets/coffee-delivery-logo.svg'
-import {CoffeeCardContainer} from './styles'
+import {CoffeeCardContainer, Tags} from './styles'
 
 export function CoffeeCard () {
     return (
         <CoffeeCardContainer>
            <img alt="teste" src={teste} />
+           <Tags>
+             <span>tradicional</span>
+             <span>com leite</span>
+           </Tags>
         </CoffeeCardContainer>
     )
 }
